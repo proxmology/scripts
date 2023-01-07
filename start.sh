6d5a0640e8da2be3b19564842607ae87e86884b3
proxmology <121979376+proxmology@users.noreply.github.com>
2023-01-07 10:26:27 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,38 +1,36 @@
 #!/usr/bin/env bash
-
-txtrst=$(tput sgr0) 
-txtred=$(tput setab 1) 
-textpurple=$(tput setab 5) 
-txtgreen=$(tput bold ; tput setaf 2) 
+function header_info {
+cat <<"EOF"
 
 
- ____                           ___  _                   
+ ____                           ___  _
 |  _ \ _ __ _____  ___ __ ___  / _ \| | ___   __ _ _   _ 
 | |_) | '__/ _ \ \/ / '_ ` _ \| | | | |/ _ \ / _` | | | |
 |  __/| | | (_) >  <| | | | | | |_| | | (_) | (_| | |_| |
 |_|   |_|  \___/_/\_\_| |_| |_|\___/|_|\___/ \__, |\__, |
                                              |___/ |___/ 
+                                             
+EOF
+}
+clear
+header_info
 
-PS3= echo "${txtred}"Desea quitar aviso suscripcion proxmox? "${txtrst}
+
+PS3="Desea quitar aviso suscripcion proxmox? "
 select suscripcion in SI NO
 do
 	break;
 done
 if [ "$suscripcion" = "SI" ]; then
-
   cp /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js.old
   sed 's/if (data.status !== 'Active') {/if (false) {/' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
   systemctl restart pveproxy.service
-
-
 echo "suscripcion quitada."
 echo ""
 echo ""
 echo ""
-
 	
 fi
-
 PS3="Desea hacer update ? "	
 select update in SI NO
 do
@@ -47,5 +45,4 @@ echo ""
 echo ""
 echo ""
 echo ""
-
 fi
