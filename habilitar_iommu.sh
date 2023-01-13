2c554a449db5960e2c81b63624f118e8e6c15a82
proxmology <121979376+proxmology@users.noreply.github.com>
2023-01-14 00:01:30 +0100

    Update habilitar_iommu.sh

--- habilitar_iommu.sh
+++ habilitar_iommu.sh
@@ -1,27 +1,76 @@
 #!/usr/bin/env bash
 
-# Comprobar si IOMMU ya está habilitado
-if [ -e /sys/kernel/iommu_groups/0/devices/0000:00:00.0 ]; then
-    echo "IOMMU ya está habilitado"
-    exit 0
-fi
+#AuthenticAMD="AMD"
+#GenuineIntel="INTEL"
 
-# Obtener información de la CPU
-CPU_VENDOR=$(cat /proc/cpuinfo | grep vendor_id | head -n1 | awk '{print $3}')
+echo "buscando si ya tiene iommu AMD"
+echo ""
+existe=` grep -l iommu=pt /etc/default/grub | wc -l`
+if [ $existe -gt 0 ];then
+    echo ""
+    echo ""
+    echo "Encontrado"
+    exit
 
-# Habilitar IOMMU para CPUs AMD
-if [ "$CPU_VENDOR" == "AuthenticAMD" ]; then
-    echo "Se ha detectado una CPU AMD, habilitando IOMMU"
-    echo "iommu=pt" >> /etc/default/grub
-    #update-grub
-    #reboot
-# Habilitar IOMMU para CPUs Intel
-elif [ "$CPU_VENDOR" == "GenuineIntel" ]; then
-    echo "Se ha detectado una CPU Intel, habilitando IOMMU"
-    echo "intel_iommu=on" >> /etc/default/grub
-    #update-grub
-    #reboot
+else
+
+    echo "NO encontrado"
+    
+    echo ""
+    echo ""
 
 fi
 
-exit 0
+echo "buscando si ya tiene iommu INTEL"
+echo ""
+existe=` grep -l intel_iommu=on /etc/default/grub | wc -l`
+if [ $existe -gt 0 ];then
+    echo ""
+    echo ""
+    echo "Encontrado"
+    exit
+
+else
+
+    echo "NO encontrado"
+
+    echo ""
+    echo ""
+fi
+
+
+
+PROC=$(grep vendor_id /proc/cpuinfo | uniq | cut -d' ' -f2)
+
+echo ""
+echo ""
+
+echo " tu procesador es: $PROC"
+
+echo ""
+echo ""
+
+#case $PROC in
+
+if [[ $1 = AuthenticAMD ]]
+then
+
+  
+               echo "usa CPU AMD, habilitando IOMMU"
+               echo "iommu=pt" >> /etc/default/grub
+               #update-grub
+               #reboot 
+               echo "realizado en AMD"
+
+  else [[ $1 = GenuineIntel ]] 
+
+
+               echo "usa CPU INTEL, habilitando IOMMU"
+               echo "intel_iommu=on" >> /etc/default/grub
+               #update-grub
+               #reboot 
+               echo "realizado en INTEL"
+
+
+fi
+
