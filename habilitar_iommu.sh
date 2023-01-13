ca5c5f5dd9fb1f7eccf0fcac2472a1ee96c97cdc
juanlusoft <86485571+juanlusoft@users.noreply.github.com>
2023-01-13 20:09:52 +0100

    Update habilitar_iommu.sh

--- habilitar_iommu.sh
+++ habilitar_iommu.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-# Comprobar si IOMMU ya está habilitado
+# Check if IOMMU is already enabled
 if [ -e /sys/kernel/iommu_groups/0/devices/0000:00:00.0 ]; then
-    echo "IOMMU ya está habilitado"
+    echo "IOMMU is already enabled"
     exit 0
 fi
 
-# Obtener información de la CPU
+# Get CPU information
 CPU_VENDOR=$(cat /proc/cpuinfo | grep vendor_id | head -n1 | awk '{print $3}')
 
-# Habilitar IOMMU para CPUs AMD
+# Enable IOMMU for AMD CPUs
 if [ "$CPU_VENDOR" == "AuthenticAMD" ]; then
-    echo "Se ha detectado una CPU AMD, habilitando IOMMU"
+    echo "AMD CPU detected, enabling IOMMU"
     echo "iommu=pt" >> /etc/default/grub
     update-grub
     reboot
-# Habilitar IOMMU para CPUs Intel
+# Enable IOMMU for Intel CPUs
 elif [ "$CPU_VENDOR" == "GenuineIntel" ]; then
-    echo "Se ha detectado una CPU Intel, habilitando IOMMU"
+    echo "Intel CPU detected, enabling IOMMU"
     echo "intel_iommu=on" >> /etc/default/grub
     update-grub
     reboot
 else
-    echo "
+    echo "Unsupported CPU vendor"
+    exit 1
+fi
