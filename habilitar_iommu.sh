b72abd6df8aeb10ae5927568f2aec8631a8ad073
juanlusoft <86485571+juanlusoft@users.noreply.github.com>
2023-01-14 10:54:04 +0100

    Add files via upload

---
+++ habilitar_iommu.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Comprobar si IOMMU ya está habilitado
+if [ -e /sys/kernel/iommu_groups/0/devices/0000:00:00.0 ]; then
+    echo "IOMMU ya está habilitado"
+    exit 0
+fi
+
+# Obtener información de la CPU
+CPU_VENDOR=$(cat /proc/cpuinfo | grep vendor_id | head -n1 | awk '{print $3}')
+
+# Habilitar IOMMU para CPUs AMD
+if [ "$CPU_VENDOR" == "AuthenticAMD" ]; then
+    echo "Se ha detectado una CPU AMD, habilitando IOMMU"
+    echo "iommu=pt" >> /etc/default/grub
+    update-grub
+    reboot
+# Habilitar IOMMU para CPUs Intel
+fi
+if [ "$CPU_VENDOR" == "GenuineIntel" ]; then
+    echo "Se ha detectado una CPU Intel, habilitando IOMMU"
+    echo "intel_iommu=on" >> /etc/default/grub
+    echo "pcie_acs_override=downstream" >> /etc/default/grub
+    update-grub
+    reboot
+else
+    echo "No se ha detectado una CPU compatible para habilitar IOMMU"
+fi
